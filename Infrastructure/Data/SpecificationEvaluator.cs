3d13423d01a576b8283c3c00be8e71f614a8f4a8
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-08-25 13:20:56 +0300

    created a specification evaluator to build query tree before excuting on database

---
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,27 @@
+using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data;
+
+public class SpecificationEvaluator<T> where T : BaseEntity
+{
+    public static IQueryable<T> GetQuery(IQueryable<T> query, ISpecification<T> spec)
+    {
+        if (spec.Creteria != null)
+        {
+            query = query.Where(spec.Creteria);
+        }
+
+        if (spec.OrderBy != null)
+        {
+            query = query.OrderBy(spec.OrderBy);
+        }
+
+        if (spec.OrderByDescending != null)
+        {
+            query = query.OrderByDescending(spec.OrderByDescending);
+        }
+
+        return query;
+    }
+}
