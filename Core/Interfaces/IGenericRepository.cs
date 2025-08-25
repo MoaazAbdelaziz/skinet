a8f3fd577e83aa6ccdf960b475871acea081fa1b
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-08-25 13:21:41 +0300

    updated generic repo interface with functions that works with specification

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -7,6 +7,8 @@ public interface IGenericRepository<T> where T : BaseEntity
     void Add(T entity);
     Task<T?> GetByIdAsync(int id);
     Task<IReadOnlyList<T>> ListAllAsync();
+    Task<T?> GetEntityWithSpec(ISpecification<T> spec);
+    Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
     void Update(T entity);
     void Delete(T entity);
     Task<bool> SaveAllAsync();
