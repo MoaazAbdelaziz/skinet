7d1f2670911515bf9f322a3ce3a416a69a1e7135
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-08-25 22:53:20 +0300

    updated app component

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Header } from "./layout/header/header";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
   title = 'Skinet';
+  products: Product[] = [];
+  private http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error)
+    });
+  }
 }
