48a5645dee708130339c2efbb89d73df48bbf566
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-08-26 18:30:16 +0300

    created neww components and updated it is routes

--- client/src/app/features/shop/product-item/product-item.ts
+++ client/src/app/features/shop/product-item/product-item.ts
@@ -6,10 +6,11 @@ import { MatCardActions } from "@angular/material/card";
 import { MatIcon } from "@angular/material/icon";
 import { CurrencyPipe } from '@angular/common';
 import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
-  imports: [MatCard, MatCardContent, MatCardActions, MatIcon, CurrencyPipe, MatButton],
+  imports: [MatCard, MatCardContent, MatCardActions, MatIcon, CurrencyPipe, MatButton, RouterLink],
   templateUrl: './product-item.html',
   styleUrl: './product-item.scss'
 })
