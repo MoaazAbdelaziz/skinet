de492ca06060cbdd2d929e2c57b01e0259fc9e89
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-08-31 01:09:24 +0300

    created login and register and the requestes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -9,13 +9,15 @@ import { Cart } from './features/cart/cart';
 import { Checkout } from './features/checkout/checkout';
 import { Login } from './features/account/login/login';
 import { Register } from './features/account/register/register';
+import { authGuard } from './core/guard/auth-guard';
+import { emptyCartGuard } from './core/guard/empty-cart-guard';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'shop', component: Shop },
     { path: 'shop/:id', component: ProductDetails },
     { path: 'cart', component: Cart },
-    { path: 'checkout', component: Checkout },
+    { path: 'checkout', component: Checkout, canActivate: [authGuard, emptyCartGuard] },
     { path: 'account/login', component: Login },
     { path: 'account/register', component: Register },
     { path: 'test-error', component: TestError },
--- client/src/app/features/account/login/login.ts
+++ client/src/app/features/account/login/login.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AccountService } from '../../../core/services/account-service';
+import { MatCard } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports:
+    [
+      ReactiveFormsModule,
+      MatCard,
+      MatFormField,
+      MatInput,
+      MatLabel,
+      MatButton
+    ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  protected accountService = inject(AccountService);
+  returnUrl = '/shop';
 
+  constructor() {
+    const url = this.activatedRoute.snapshot.queryParamMap.get('returnUrl');
+    if (url) this.returnUrl = url;
+  }
+
+  loginForm = this.fb.group({
+    email: [''],
+    password: ['']
+  });
+
+  onSubmit() {
+    this.accountService.login(this.loginForm.value).subscribe({
+      next: () => {
+        this.accountService.getUserInfo().subscribe();
+        this.router.navigateByUrl(this.returnUrl);
+      },
+    })
+  }
 }
